13d321067d70fad497d2eb38eaa415890f8155ba
Den <codeclass.ru@gmail.com>
2022-09-07 12:36:35 +0300

    initial commit

---
+++ api/api.ts
@@ -0,0 +1,5 @@
+export * from './default.service';
+import { DefaultService } from './default.service';
+export * from './_.service';
+import { _Service } from './_.service';
+export const APIS = [DefaultService, _Service];
